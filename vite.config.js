e071ea5b9938962f5cdb5fdbea335b00ef18b105
Syafizam <crawlernight548@gmail.com>
2026-02-04 19:42:46 +0800

    Fix ralat react-refresh dan guna plugin-react yang betul

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh' // atau @vitejs/plugin-react
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/HackDev-Platform/', // TAMBAH BARIS INI
+  base: '/HackDev-Platform/', 
 })
