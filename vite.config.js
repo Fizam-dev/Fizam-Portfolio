979be95dadca3d9fe0c53053d45cab322218cb41
Syafizam <crawlernight548@gmail.com>
2026-02-04 19:37:34 +0800

    insyaallah boleh

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/react-refresh' // atau @vitejs/plugin-react
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/HackDev-Platform/', // TAMBAH BARIS INI
 })
